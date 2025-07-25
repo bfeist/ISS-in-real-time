1577d1c94875ca2c17cc744cb7c31e6dc46ca85e
Ben Feist <benjamin.f.feist@nasa.gov>
2025-07-25 12:33:57 -0400

    removed unused routes; new timelineDayContainer initial component

---
+++ src/components/timelineDay/timelineDayContainer.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import styles from "./timelineDayContainer.module.css";
+
+const TimelineDayContainer: FunctionComponent = (): JSX.Element => {
+  return <div className={styles.container}>{/* Timeline day content goes here */}</div>;
+};
+
+export default TimelineDayContainer;
--- src/index.tsx
+++ src/index.tsx
@@ -9,21 +9,12 @@ import {
 } from "react-router-dom";
 import App from "./App.tsx";
 import Home from "pages/index.tsx";
-import DatePage from "pages/dateSlug";
-import LayoutTest from "pages/layout_test.tsx";
-import IndexOld from "pages/index_old.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Home />} />
       <Route path=":dateTimeSlug" element={<Home />} />
-      <Route path="date/:date" element={<DatePage />} />
-      <Route
-        path="layout_test"
-        element={<LayoutTest selectedDate="2020-01-01" setSelectedDate={() => {}} />}
-      />
-      <Route path="index_old" element={<IndexOld />} />
     </Route>
   )
 );
