8994f8e0be47851c0e433c848e1f3b9c2163a1ba
Ben Feist <benjamin.f.feist@nasa.gov>
2025-10-05 23:00:02 -0400

    ncu major upgrades; eslint-plugin-react-hooks config changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
 import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
+import reactHooksPkg from "eslint-plugin-react-hooks";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import jsxa11y from "eslint-plugin-jsx-a11y";
 import prettier from "eslint-plugin-prettier";
@@ -11,6 +11,8 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+
+const { configs: reactHooksConfigs } = reactHooksPkg;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -24,14 +26,12 @@ export default [
   {
     ignores: ["**/public/**/*", "**/coverage", "**/.local", "coverage/*", "**/.venv"],
   },
-  ...fixupConfigRules(
-    compat.extends("prettier", "plugin:react-hooks/recommended", "plugin:jsx-a11y/recommended")
-  ),
+  ...fixupConfigRules(compat.extends("prettier", "plugin:jsx-a11y/recommended")),
+  ...reactHooksConfigs.recommended,
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
     plugins: {
       react,
-      "react-hooks": fixupPluginRules(reactHooks),
       "@typescript-eslint": typescriptEslint,
       prettier,
       jsxa11y,
